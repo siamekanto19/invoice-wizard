3ad0a02eda3441b37771aedc801e98c8d01d8b96
Siam <siamekanto360@gmail.com>
2025-09-01 08:29:24 +0600

    update total amount container width

--- src/components/invoice/templates/ProfessionalTemplate.tsx
+++ src/components/invoice/templates/ProfessionalTemplate.tsx
@@ -756,7 +756,7 @@ const InvoiceDocument = ({ data, previewMode = false }: Props) => {
         {/* Summary */}
         <View
           style={{
-            width: 140,
+            width: 180,
             marginLeft: "auto",
             paddingTop: 10,
             paddingBottom: 10,
